d46df7bd4e7164657d3a83f9b139f2aece9bfb5a
rikhi.ram <rikhi.ram@qovarian.com>
2022-10-20 14:16:49 +0530

    initial commit with proejct structure

---
+++ src/main/java/com/sudoku/app/SudokuApplication.java
@@ -0,0 +1,13 @@
+package com.sudoku.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudokuApplication.class, args);
+    }
+
+}
